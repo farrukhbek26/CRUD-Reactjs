09cee688c5d71543ac5cc34d43c2816244902dd7
farrukh26 <98838148+farrukhbek26@users.noreply.github.com>
2023-04-19 17:12:53 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,106 @@
+import React from "react";
+import {student} from './Student'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 1,
+      data: student,
+      title: 'WebBrain Academy',
+      name: '',
+      status: '',
+      active: null,
+    }
+  }
+
+  render() {
+   const onFilter = (e) => {
+
+    let res = student.filter((value) => value.name.includes(e.target.value));
+    this.setState({
+      data: res,
+    })
+   };
+   const onDelete = (id) => {
+    console.log(id);
+    let res = this.state.data.filter((value) => value.id !== id);
+    this.setState({data: res});
+   }
+   const onChange = (event) => {
+
+
+    this.setState({[event.target.name] : event.target.value});
+   }
+    const onAdd =() =>{
+      let user = {
+        id: Date.now(),
+        name: this.state.name,
+        status: this.state.status,
+      };
+      this.setState({
+        data: [...this.state.data, user],
+        name: '', 
+        status: ''
+      })
+      console.log(user);
+
+    }
+    const onEdit = ({id, name, status}, isSave)  => {
+      if(isSave){
+        let res = this.state.data.map((value) => value.id === this.state.active.id ? {...value, name: this.state.name, status: this.state.status}: value)
+        this.setState({active:null, data: res});
+      } else {
+      this.setState({
+        name: name,
+        status: status,
+        active: {id, name, status},
+      })
+    }
+    }
+    
+   
+   
+    return (
+      <div>
+        <h1>name: {this.state.name}</h1>
+        <h1>status: {this.state.status}</h1>
+        <input name="name" onChange={onChange} type="text" placeholder="name" />
+        <input name="status" onChange={onChange} type="text" placeholder="status" />
+        <button onClick={onAdd}>Add</button>
+        <hr />
+        <input onChange={onFilter} type="text" placeholder="Filter" />
+        <table border='1' width='100%'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Status</th>
+              <th>Delete</th>
+              <th>Edit</th>
+            </tr>
+          </thead>
+          <tbody>
+        
+        {
+          this.state.data.map(({id, name, status})=>{
+            return(
+              <tr key={id}>
+                <td>{id}</td>
+                <td>{this.state.active?.id === id ? <input onChange={onChange} name='name' value={this.state.name} type="text" /> : name}</td>
+                <td>{this.state.active?.id === id ? <input onChange={onChange} name='status' value={this.state.status} type="text" /> : status}</td>
+                <td><button onClick={() => onDelete(id)}>delete</button></td>
+                <td><button onClick={() => onEdit({id, name, status},this.state.active?.id === id )}>{this.state.active?.id === id ? 'save' : 'edit'}</button></td>
+              </tr>
+            )
+          })
+        }
+        </tbody>
+        </table>
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/Student.jsx
@@ -0,0 +1,10 @@
+export const student = [
+    {id: 1, name: 'Fara', status: 'IT'},
+    {id: 2, name: 'Bobur', status: 'Tadbirkor'},
+    {id: 3, name: 'Ziyod', status: 'Dizayner'},
+    {id: 4, name: 'Zarif', status: 'Talaba'},
+    {id: 5, name: 'Nodir', status: 'Bekorchi'},
+    {id: 6, name: 'Jurabek', status: 'Dasturchi'},
+    {id: 7, name: 'Jamshid', status: 'Arxitektor'}
+    
+]
